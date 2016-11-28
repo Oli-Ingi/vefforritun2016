191a10aadc8033a1d93220083cb784973bae2a84
Ólafur Ingi Guðmundsson <lafur Ingi Gumundsson>
2016-11-28 15:40:43 +0000

    Linking frá main page yfir á einn thread

--- routes/thread.js
+++ routes/thread.js
@@ -2,8 +2,16 @@ const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('thread', { title: 'Express' });
+router.get('/:name', (req, res) => {
+  const threadTitle = capitalize(req.params.name);
+
+  // DBManager(title); get all that is needed from DBManager to render page
+  res.render('thread', { threadTitle });
 });
+
+function capitalize(str) {
+  if (str)
+    return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
+};
 
 module.exports = router;
