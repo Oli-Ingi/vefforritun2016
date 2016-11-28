67521c2839db51a38c2086ea64965cdd34204e24
Wolfcoder13 <Wolfninja13@gmail.com>
2016-11-28 21:04:14 +0000

    stil not done, but very close

--- DBManager.js
+++ DBManager.js
@@ -1,11 +1,33 @@
 const pgp = require('pg-promise')();
 
-const db = pgp('postgres://Wolfster:asdf@localhost:5432/forum');
+const db = pgp('postgres://forum:forum@localhost:5432/forum');
 
+Thread (threadName, author, text, date = 0, id = 0) => {
+  this.id = id;
+  this.threadName = threadName;
+  this.author = author;
+  this.text = text;
+  this.date = date;
+}
 
+Post (threadId, postName, author, text, id = 0, date = 0) => {
+  this.id = id;
+  this.threadId = threadId;
+  this.postName = postName;
+  this.author = author;
+  this.text = text;
+  this.date = date;
+}
 
-let getPosts = () => {
-  let postData;
+Reply (postId, author, text, id = 0, date = 0) => {
+  this.id = id;
+  this.postId = postId;
+  this.author = author;
+  this.text = text;
+  this.date = date;
+}
+
+let getThreads = () => {
   return db.any("select * from threads")
     .then((data) => {
       return data;
@@ -13,19 +35,57 @@ let getPosts = () => {
     .catch((error) => {
         console.log("neibb");
         return "no";
-    })
-
+    });
 };
 
-let getReplies = (id) => {
-  return db.any("select * from replies where postId = $1", id)
+// returns an array of Post objects
+let getPostsInThread = (threadId) => {
+  return db.any("select * from posts where threadId = $1", threadId)
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+        console.log("neibb");
+        return "no";
+    });
+};
+
+let getRepliesInPost = (postId) => {
+  return db.any("select * from replies where postId = $1", postId)
     .then((data) => {
         return data;
     })
     .catch((error) => {
         console.log("neibb");
         return "no";
+    });
+};
+
+let saveThread = (thread) => {
+  let threadName = thread.threadName,
+      author = thread.author,
+      text = thread.text;
+  return db.none("INSERT INTO posts(threadId, postName, author, text) VALUES($1, $2, $3, $4);", [threadId, postName, author, text])
+    .then((data) => {
+        console.log("successful");
     })
+    .catch((error) => {
+        console.log("neibb");
+    });
+};
+
+let savePost = (post) => {
+  let threadId = post.threadId,
+      postName = post.postName,
+      author = post.author,
+      text = post.text;
+  return db.none("INSERT INTO posts(threadId, postName, author, text) VALUES($1, $2, $3, $4);", [threadId, postName, author, text])
+    .then((data) => {
+        console.log("successful");
+    })
+    .catch((error) => {
+        console.log("neibb");
+    });
 };
 
 // reply has to have the properties postId, author and text.
@@ -45,34 +105,43 @@ let saveReply = (reply) => {
 };
 
 
-// Just to test the functions
-let texti = getPosts();
+// START OF TESTING
+//---------------------------
+let texti = getPostsInThread(1);
 texti.then( data => {
-  // console.log(texti);
   console.log(data);
   // console.log(data[0].text);
   // console.log(data[1].text);
 });
 
 // just to test the functions
-let svar = getReplies(1);
+let svar = getRepliesInPost(1);
 svar.then( data => {
   console.log(data);
 }).catch(e => { console.log(e); });
 
 
-let reply = {
-  postId: 1,
-  author: "Mr. Bombastic",
-  text: "well GTA is amazing..."
-}
-
+let reply = new Reply(1, "Mr. Bombastic", "this seems to work");
 let insert = saveReply(reply);
 insert.then( (data) => {
   console.log(data);
   console.log("virkar þetta");
 });
 
-module.exports.getPosts;
-module.exports.getReplies;
+let post = new Post(1, "this about bazookas", "Mr. Bombastic", "this text is describing things about using TNT instead of rockets.");
+let insert2 = savePost(post);
+insert2.then( () => {
+  console.log("virkar þetta");
+});
+
+
+
+module.exports.Thread;
+module.exports.Post;
+module.exports.Reply;
+module.exports.getThreads;
+module.exports.getPostsInThread;
+module.exports.getRepliesInPost;
+module.exports.saveThread;
+module.exports.savePost;
 module.exports.saveReply;
