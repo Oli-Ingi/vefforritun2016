8b9d42aaa82564ecff82a76c648a74baa8fb97a3
Wolfcoder13 <Wolfninja13@gmail.com>
2016-11-28 20:08:44 +0000

    more stuff in DBManager now

--- DBManager.js
+++ DBManager.js
@@ -1,14 +1,13 @@
 const pgp = require('pg-promise')();
 
-const db = pgp('postgres://vef:vef@localhost:5432/forum');
+const db = pgp('postgres://Wolfster:asdf@localhost:5432/forum');
 
 
 
-getPosts = () => {
+let getPosts = () => {
   let postData;
   return db.any("select * from threads")
     .then((data) => {
-      // console.log(data);
       return data;
     })
     .catch((error) => {
@@ -18,8 +17,8 @@ getPosts = () => {
 
 };
 
-getReplies = (id) => {
-  return db.any("select * from replies where threadId = $1", id)
+let getReplies = (id) => {
+  return db.any("select * from replies where postId = $1", id)
     .then((data) => {
         return data;
     })
@@ -29,17 +28,51 @@ getReplies = (id) => {
     })
 };
 
+// reply has to have the properties postId, author and text.
+// postId has to point to an id of a postId
+// author and text can just be strings.
+let saveReply = (reply) => {
+  let postId = reply.postId,
+      author = reply.author,
+      text = reply.text;
+  return db.none("INSERT INTO replies(postId, author, text) VALUES($1, $2, $3);", [postId, author, text])
+    .then((data) => {
+        console.log("successful");
+    })
+    .catch((error) => {
+        console.log("neibb");
+    })
+};
+
+
+// Just to test the functions
 let texti = getPosts();
 texti.then( data => {
   // console.log(texti);
   console.log(data);
-  console.log(data[0].text);
+  // console.log(data[0].text);
+  // console.log(data[1].text);
 });
 
+// just to test the functions
 let svar = getReplies(1);
 svar.then( data => {
   console.log(data);
 }).catch(e => { console.log(e); });
 
+
+let reply = {
+  postId: 1,
+  author: "Mr. Bombastic",
+  text: "well GTA is amazing..."
+}
+
+let insert = saveReply(reply);
+insert.then( (data) => {
+  console.log(data);
+  console.log("virkar þetta");
+});
+
 module.exports.getPosts;
 module.exports.getReplies;
+module.exports.saveReply;
