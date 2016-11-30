e3c41ec16d9ef998138bf9543d2ac3b6cecff3b1
Wolfcoder13 <Wolfninja13@gmail.com>
2016-11-30 18:30:19 +0000

    it now return ID when you save

--- DBManager.js
+++ DBManager.js
@@ -54,13 +54,11 @@ const saveThread = (thread) => {
   const threadName = thread.threadName;
   const author = thread.author;
   const text = thread.text;
-  return db.none('INSERT INTO threads(threadName, author, text) VALUES($1, $2, $3);', [threadName, author, text])
+  return db.one('INSERT INTO threads(threadName, author, text) VALUES($1, $2, $3) returning id', [threadName, author, text])
     .then((data) => {
-      console.log('successful');
-      console.log(data);
+      console.log(data.id);
     })
     .catch((error) => {
-      console.log('neibb');
       console.log(error);
     });
 };
@@ -70,13 +68,11 @@ const savePost = (post) => {
   const postName = post.postName;
   const author = post.author;
   const text = post.text;
-  return db.none('INSERT INTO posts(threadId, postName, author, text) VALUES($1, $2, $3, $4);', [threadId, postName, author, text])
+  return db.one('INSERT INTO posts(threadId, postName, author, text) VALUES($1, $2, $3, $4) returning id', [threadId, postName, author, text])
   .then((data) => {
-    console.log('successful');
-    console.log(data);
+    console.log(data.id);
   })
   .catch((error) => {
-    console.log('neibb');
     console.log(error);
   });
 };
@@ -93,14 +89,11 @@ const saveReply = reply => {
   console.log(author);
   console.log(text);
   console.log("-----------------------------");
-  return db.none('INSERT INTO replies(postId, author, text) VALUES($1, $2, $3);', [postId, author, text])
+  return db.one('INSERT INTO replies(postId, author, text) VALUES($1, $2, $3) returning id', [postId, author, text])
     .then((data) => {
-      console.log('successful');
-      console.log(data);
+      console.log(data.id);
     })
     .catch((error) => {
-      console.log('neibb');
-      // console.log(error);
     });
 };
 
@@ -109,32 +102,34 @@ const saveReply = reply => {
 //---------------------------
 const texti = getPostsInThread(1);
 texti.then((data) => {
-  console.log(data);
+  console.log("--");
 });
 
 // just to test the functions
 const svar = getRepliesInPost(1);
 svar.then((data) => {
-  console.log(data);
+  console.log("--");
 }).catch((e) => { console.log(e); });
 
 const thread = new Thread('this about bazookas', 'Mr. Bombastic', 'this text is describing things about using TNT instead of rockets.');
 const insert3 = saveThread(thread);
-insert3.then(() => {
-  console.log('virkar þetta');
+insert3.then((data) => {
+  console.log(data.id);
+  console.log('virkar að vista Thread');
 });
 //
 const post = new Post(1, 'this about bazookas', 'Mr. Bombastic', 'this text is describing things about using TNT instead of rockets.');
 const insert2 = savePost(post);
-insert2.then(() => {
-  console.log('virkar þetta');
+insert2.then((data) => {
+  console.log(data.id);
+  console.log('virkar að vista post');
 });
 
 const reply = new Reply(1, 'Mr. Bombastic', 'this seems to work');
 const insert = saveReply(reply);
 insert.then((data) => {
-  console.log(data);
-  console.log('virkar þetta');
+  console.log(data.id);
+  console.log('virkar að vista reply');
 });
 
 
